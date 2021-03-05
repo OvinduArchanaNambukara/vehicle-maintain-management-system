e7e54be018a40364025725ce027d6fe73a2d11b3
ovindu <ovindunambukara@gmail.com>
2021-03-05 21:05:49 +0530

    add qr code generator

--- src/app/qrcode/qrcode.component.ts
+++ src/app/qrcode/qrcode.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-qrcode',
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QrcodeComponent implements OnInit {
 
-  constructor() { }
+  public myAngularxQrCode = 'Vehicle-MM-System';
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
-
 }
