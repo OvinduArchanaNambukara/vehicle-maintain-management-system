d7c1906f4629ac40c7d2efaefcc2b65b794f2ff4
ovindu <ovindunambukara@gmail.com>
2021-03-14 01:14:47 +0530

    add limit and only numbers to credit card number and cvv

--- src/app/booking-from/booking-from.component.ts
+++ src/app/booking-from/booking-from.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-booking-from',
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookingFromComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  OnlyNumberAccept(event: any): boolean {
+    const charcode = (event.which) ? event.which : event.keyCode;
+    if (charcode > 32 && (charcode < 48 || charcode > 57)) {
+      return false;
+    }
+    return true;
+  }
 }
